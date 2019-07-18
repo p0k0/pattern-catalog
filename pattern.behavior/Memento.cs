81c35bcae79b49e813fed109cbff10f50a4cec97
p0k0 <m11e@ya.ru>
2019-07-18 12:41:43 +0300

    memento

---
+++ pattern.behavior/Memento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace pattern.behavior
+{
+    class State
+    {
+    }
+
+    class Memento
+    {
+        private Memento() {}
+
+        private State _state;
+        protected State State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+        
+        class Originator
+        {
+            public Memento CreateMemento()
+            {
+                return new Memento() { State = _state };
+            }
+
+            public void SetMemento(Memento memento)
+            {
+                _state = memento.State;
+            }
+
+            private State _state;
+        }
+    }
+}
